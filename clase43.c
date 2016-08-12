c4f46f3e50d753535807ad8eb1acf08ba3f8364c
yeny-gomez <yeny-gomez@users.noreply.github.com>
2016-08-12 13:48:24 -0400

    Update clase43.c

--- clase43.c
+++ clase43.c
@@ -2,16 +2,16 @@
 #include<conio.h>
 #include<stdlib.h>
 
-
-
-
 void main(){
 	
 	int x1,x2,x3,x4,y1,y2,y3,y4,calculo,validar=0;
+	float punto_x_reinicio,punto_y_reinicio,punto_x,punto_y,numero_x,numero_y;
+	char resp;
+	
 	
 	do{
 		
-			printf("\n Ingresar punto 1:");
+	printf("\n Ingresar punto 1:");
 	 printf("\n x1: ");
 	  scanf("%i",&x1);
 	  	printf("\n y1: ");
@@ -49,18 +49,69 @@ void main(){
       }
 	
 	  if( x1 != x3 ){
-	  	printf("\n  no es una recta vertical 1");
+	  	printf("\n  no es una recta vertical izquierda");
 	  		validar=1;
 	  }
 	   
 	   if( x2 != x4){
-	   	printf("\n  no es una recta vertical 2");
+	   	printf("\n  no es una recta vertical derecha");
 	   		validar=1;
 	   }
-		
-		
-		
+		///----solo para validarel cuadrado
 	}while( validar == 1);
 
-	
+
+       punto_x=( (x2-x1)/2)+(x1);
+       punto_x_reinicio=punto_x;
+       punto_y=( (y3-y1)/2)+(y1);
+       punto_y_reinicio=punto_y;
+   
+   printf("\n\n   coordenada del punto: x=%.2f y=%.2f", punto_x, punto_y);
+//---------------jugar
+
+        do{ 
+		
+		   printf("\n\nIngrese un numero para mover el punto en x: ");
+            scanf("%f",&numero_x);
+           printf("\n\nIngrese un numero para mover el punto en y: ");
+            scanf("%f",&numero_y);
+     
+             punto_x=punto_x+numero_x;
+             punto_y=punto_y+numero_y;
+       
+          printf("\n\n  la nueva coordenada del punto: x=%.2f y=%.2f", punto_x, punto_y);
+       
+       if( punto_x > x2){
+			printf("\n\nSe sale del cuadro, se reinicia el juego");
+			punto_x= punto_x_reinicio;
+			punto_y= punto_y_reinicio;
+			   printf("\n\n  la nueva coordenada del punto: x=%.2f y=%.2f", punto_x, punto_y);
+       }
+        if( punto_x < x1){
+			printf("\n\nSe sale del cuadro, se reinicia el juego");
+			punto_x= punto_x_reinicio;
+			punto_y= punto_y_reinicio;
+			   printf("\n\n  la nueva coordenada del punto: x=%.2f y=%.2f", punto_x, punto_y);
+       }
+        
+        if( punto_y > y3){
+			printf("\n\nSe sale del cuadro, se reinicia el juego");
+			punto_y= punto_y_reinicio;
+			punto_y= punto_y_reinicio;
+			   printf("\n\n  la nueva coordenada del punto: x=%.2f y=%.2f", punto_x, punto_y);
+       }
+        if( punto_y < y1){
+			printf("\n\nSe sale del cuadro, se reinicia el juego");
+			punto_y= punto_y_reinicio;
+			punto_y= punto_y_reinicio;
+			   printf("\n\n  la nueva coordenada del punto: x=%.2f y=%.2f", punto_x, punto_y);
+       }
+		
+		printf("\n\nDesea seguir jugando (s/n): ");
+		fflush(stdin);
+		 scanf("%c",&resp);
+		 resp=tolower(resp);
+		 
+	     }while(resp == 's');
+
 }
